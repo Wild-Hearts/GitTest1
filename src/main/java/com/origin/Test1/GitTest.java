d59bfe393df30b876f7f75d4124b07ab064445f9
originPark <87929552+Wild-Hearts@users.noreply.github.com>
2023-04-05 15:56:38 +0800

    hot fix2

--- src/main/java/com/origin/Test1/GitTest.java
+++ src/main/java/com/origin/Test1/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("gitTest111");
         System.out.println("gitTest222");
         System.out.println("进行热修复");
+        System.out.println("第二次热修复");
     }
 }
